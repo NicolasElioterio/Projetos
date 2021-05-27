3c8e129abfda6e01a80ed2bbaf0aae444a2966fe
NicolasElioterio <nicolaselioteriojf@gmail.com>
2021-05-27 19:21:31 -0300

    ...
    ...

---
+++ Tela-de-login/scripts/script.js
@@ -0,0 +1,7 @@
+var email, senha, enviar, cadastro;
+
+email = document.getElementById('email');
+senha = document.getElementById('senha');
+enviar = document.getElementById('enviar');
+cadastro = document.getElementById('cadastro');
+
